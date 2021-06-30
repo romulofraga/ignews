235b6b4fc4881976091296a81581a1ceb091ae1b
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2021-06-29 21:20:43 -0300

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "./home.module.scss";
+
+import avatarImg from "../../public/images/avatar.svg";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title> Início | ig.news</title>
+        <title> Home | ig.news</title>
       </Head>
-      <h1>Hello Word</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br /> the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <Image src={avatarImg} alt="Girl coding" />
+      </main>
     </>
   );
 }
